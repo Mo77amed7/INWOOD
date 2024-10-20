6a976df6051f0aa3a6383dc976050d61494e4c60
Mohamed <99067002+Mo77amed7@users.noreply.github.com>
2024-10-20 15:50:41 +0300

    Update living-room.js

--- js/living-room.js
+++ js/living-room.js
@@ -6,22 +6,24 @@ fetch("js/all-products.json")
   })
   .catch((error) => console.error("Error fetching data:", error));
 
-
 function displayProducts(products, livingrooms) {
   const productsDiv = document.getElementById(livingrooms);
   products.forEach((product) => {
     const productDiv = document.createElement("div");
     productDiv.classList.add("col-12", "col-md-6", "col-lg-4");
-
-    productDiv.innerHTML = `
-        <div class="product card">
+    const productCard = document.createElement("div");
+    productCard.classList.add("product");
+    productDiv.appendChild(productCard);
+    productCard.setAttribute("data-id", product.id);
+    productCard.setAttribute("data-name", product.title);
+    productCard.setAttribute("data-price", product.price);
+    productCard.setAttribute("data-image", product.img);
+    productCard.setAttribute("data-description", product.desc);
+    productCard.innerHTML = `
           <span class="discount">${product.disc}</span>
-          <span class="wish-icon"><i class="fa-solid fa-heart"></i></span>
-          <div class="image">
+          <button class="add-to-favorites"><i class="fa-solid fa-heart"></i></button>          <div class="image">
             <a href="#">
-              <img class="img-fluid" src="${product.img}" alt="${
-      product.title
-    }" />
+              <img class="img-fluid" src="${product.img}" alt="${product.title}" />
             </a>
           </div>
           <div class="desc">
@@ -29,11 +31,21 @@ function displayProducts(products, livingrooms) {
               <h6>${product.title}</h6>
               <p class="price">$${product.price}</p>
             </div>
-            <a href="#"><i class="fa-solid fa-cart-shopping"></i></a>
-          </div>
+            <button class="add-to-cart">
+            <i class="fa-solid fa-cart-shopping"></i></button>
         </div>
       `;
 
     productsDiv.appendChild(productDiv);
+
+    // Attach event listeners
+    productCard
+      .querySelector(".add-to-cart")
+      .addEventListener("click", () => addToCart(getProductInfo(productCard)));
+    productCard
+      .querySelector(".add-to-favorites")
+      .addEventListener("click", () =>
+        addToFavorites(getProductInfo(productCard))
+      );
   });
 }
